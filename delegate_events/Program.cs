57a10f9822d210be2a0e46066dc00072b723a4fb
F0sya <nikitachmyria@gmail.com>
2025-03-21 16:37:29 +0200

    Add project files.

---
+++ delegate_events/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace delegate_events
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
