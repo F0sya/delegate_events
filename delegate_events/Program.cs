2e5c62886d169a596a6934cc5fc70219f8d7ef72
F0sya <nikitachmyria@gmail.com>
2025-03-21 17:05:02 +0200

    Sixth practice task is completed

--- delegate_events/Program.cs
+++ delegate_events/Program.cs
@@ -1,10 +1,11 @@
 ﻿namespace delegate_events
 {
-    internal class Program
+    public class Checker
     {
+        public Func<int[], int> Minimum = x => x.Min();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
         }
     }
 }
